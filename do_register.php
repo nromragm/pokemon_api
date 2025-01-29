c69951716f7001d890467cf8823710bef75c1522
nromragm <mgarmorn@g.educaand.es>
2025-01-29 21:26:09 +0100

    a

--- do_register.php
+++ do_register.php
@@ -30,28 +30,5 @@
         <h4 style="color: red"> <?= isset($register_incorrecto) ? 'Register incorrecto' : ''?></h4>
     </div>
     </main>
-    <main>
-    <div class="form-container">
-        <h2>Registro de usuario</h2>
-        <form action="funcionRegister.php" method="post">
-            <label for="nombre">Nombre</label>
-            <input type="text" name="nombre" required style="border-color: <?= $register_incorrecto ? 'red' : '' ?> " value="<?= isset($nombre) ? $nombre : '' ?>">
-            <br>
-            <label for="email">Email</label>
-            <input type="email" name="email" required style="border-color: <?= $register_incorrecto ? 'red' : '' ?> " value="<?= isset($email) ? $email : '' ?>">
-            <br>
-            <label for="password">Contraseña</label>
-            <input type="password" name="password" required style="border-color: <?= $register_incorrecto ? 'red' : '' ?> " value="<?= isset($password) ? $password : '' ?>">
-            <br>
-            <label for="password2">Repite la contraseña</label>
-            <input type="password" name="password2" required style="border-color: <?= $register_incorrecto ? 'red' : '' ?> " value="<?= isset($password2) ? $password2 : '' ?>">
-            <br>
-            <input type="submit" value="Registrarse">
-        </form>
-
-        <h3>¿Ya tienes una cuenta? <br> <a href="do_login.php">Inicia Sesion aqui</a></h3>
-        <h4 style="color: red"> <?= isset($register_incorrecto) ? 'Register incorrecto' : ''?></h4>
-    </div>
-    </main>
 </body>
 </html>
